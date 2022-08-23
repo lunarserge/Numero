eab29f625c5d91e249cb8e8df0d6c8716f5a0c1c
Serge Lunev <lunarserge@gmail.com>
2022-08-23 11:32:18 -0700

    address code review feedback, also upgrade install-boost in Actions

--- test/t_1/main.py
+++ test/t_1/main.py
@@ -2,7 +2,7 @@
 
 from env import *
 
-output_ref  = "No input dates file is selected. Exiting.\n\n" + open(test_root+"/help.txt").read()
+output_ref  = "No input dates file is selected. Exiting.\n\n" + open(test_root+os.path.sep+"help.txt").read()
 output_test = subprocess.run(numero, capture_output=True, text=True).stderr
 
 sys.exit(output_ref != output_test)
